25e1613c85e15f6672750fa7fa98e5f39170ce24
unknown <hpcaparas@gmail.com>
2023-09-10 03:29:31 -0400

    2023-09-10 added webconfig

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
-export const API_URL_1 = 'http://localhost:8081/'
-export const API_URL = 'https://apautomation-backend.azurewebsites.net/'
+export const API_URL = 'http://localhost:8081/'
+export const API_URL_2 = 'https://apautomation-backend.azurewebsites.net/'
