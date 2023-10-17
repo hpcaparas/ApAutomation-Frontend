601531aec4929ae793d471a9ceee24335e68719b
unknown <hpcaparas@gmail.com>
2023-10-17 05:14:02 -0400

    updated UI

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://192.168.43.21:8081/'
-export const API_URL_ = 'https://apautomation-backend.azurewebsites.net/'
+export const API_URL_ = 'http://192.168.43.21:8081/'
+export const API_URL = 'https://apautomation-backend.azurewebsites.net/'
